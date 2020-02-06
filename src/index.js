472f46378d11ad7775ae1b45e53471656f520434
Admin <admin@Admins-iMac.local>
2020-02-06 20:37:48 +0400

    initial commit >>project is created

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Post from './Post';
+import json from './assets/myJSON';
+import logo from './assets/logo.jpg';
+import './styles/style.less';
+import './styles/style.scss';
+import './styles/style.css';
+
+
+const post = new Post("hellooo",logo)
+console.log("post to string",post.toString());
+console.log('jjjj:', json);
+console.log("1:16");
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js',
+        analytics: './analytics.js'
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['css-loader', 'less-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
