9c44dd151369e935245bc940dc103d3e7638ee8c
Seda Tathevik Hayrapetyan <sedahayrapetyan@Sedas-MacBook-Pro.local>
2020-02-07 14:04:46 +0400

    hot server is created on 4200 port and CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
@@ -12,11 +14,20 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer:{
+        port: 4200
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            { 
+                from: path.resolve(__dirname, 'src/favicon.ico'), 
+                to: path.resolve(__dirname, 'dist')
+            },
+          ])
     ],
     module: {
         rules: [
